88bdbb01e7449b14632a668c7d75d2770791dfd3
senthilnathanstrackit <senthilnathan.e@strackit.com>
2025-12-29 20:32:15 +0530

    fix

--- src/modules/shop/index.js
+++ src/modules/shop/index.js
@@ -1,6 +1,5 @@
-import { fetchShops, updateShop } from './controller/index.js';
+import { fetchShops } from './controller/index.js';
 
 export default {
-  fetchShops,
-  updateShop,
+  fetchShops
 };
