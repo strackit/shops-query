de76d975583305236af37a13070567826d8bf99c
senthilnathanstrackit <senthilnathan.e@strackit.com>
2026-05-07 15:51:13 +0530

    specification dropdown field in cart and order page (#56)

--- src/modules/cart/mutations/add.js
+++ src/modules/cart/mutations/add.js
@@ -6,23 +6,26 @@ export const ADD_TO_CART = gql`
     $productId: Int!
     $shopId: Int!
     $quantity: Int!
+    $customSpecification: GraphQLJSON
   ) {
     Cart(
       userId: $userId
       productId: $productId
       shopId: $shopId
       quantity: $quantity
+      customSpecification: $customSpecification
     ) {
       id
       productId
       userId
       shopId
       quantity
+      customSpecification
     }
   }
 `;
 
-export const addToCart = async ({ productId, shopId, userId, quantity = 1 }) => {
+export const addToCart = async ({ productId, shopId, userId, quantity = 1, customSpecification = {} }) => {
   try {
     const { data, errors } = await client.mutate({
       mutation: ADD_TO_CART,
@@ -30,7 +33,8 @@ export const addToCart = async ({ productId, shopId, userId, quantity = 1 }) => 
         userId: Number(userId),
         productId: Number(productId),
         shopId: Number(shopId),
-        quantity: Number(quantity)
+        quantity: Number(quantity),
+        customSpecification: customSpecification || {}
       }
     });
 
