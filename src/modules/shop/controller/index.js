a3799caa7c187c613497858d302b99d8df152057
senthilnathanstrackit <senthilnathan.e@strackit.com>
2026-08-06 18:11:59 +0530

    Meta pixel implementation (#62)
    * Meta pixel implementation
    
    * rename function name
    
    ---------
    
    Co-authored-by: senthilnathan <developersenthil5@gmail.com>

--- src/modules/shop/controller/index.js
+++ src/modules/shop/controller/index.js
@@ -1,5 +1,10 @@
 import { GET_SHOP_LIST } from '../queries/get.js';
+import { TRACK_SERVER_META_EVENT } from '../mutation/post.js';
 
 export async function fetchShops(filter) {
   return GET_SHOP_LIST(filter);
+}
+
+export async function postTrackMetaEvent(shopId, eventName, eventId, customData, userData) {
+  return TRACK_SERVER_META_EVENT(shopId, eventName, eventId, customData, userData);
 }
--- src/modules/shop/index.js
+++ src/modules/shop/index.js
@@ -1,5 +1,6 @@
-import { fetchShops } from './controller/index.js';
+import { fetchShops, postTrackMetaEvent } from './controller/index.js';
 
 export default {
-  fetchShops
+  fetchShops,
+  postTrackMetaEvent
 };
